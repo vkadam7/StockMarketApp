7483fe5a46ceb0ee989dde41ecada0ccd63ee8aa
miqdadhafiz35 <98671358+miqdadhafiz35@users.noreply.github.com>
2022-09-22 14:39:43 -0400

    log in works just have to add the session to keep user logged in and get info from database

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, session, render_template, request, redirect
+from flask import Flask, session, render_template, request, redirect, url_for
 import pyrebase
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+dbfire = firestore.client() #firestore database
 
 app = Flask(__name__)
 
@@ -16,10 +24,13 @@ config = {
 
 firebase = pyrebase.initialize_app(config)
 authen = firebase.auth()
+db1 = firebase.database()
 
 app.secret_key = "aksjdkajsbfjadhvbfjabhsdk"
 
+persons = {"logged_in": False,"uName": "", "uEmail": "", "uID": ""}
 
+"""
 class RegistrationForm(FlaskForm):
     name = StringField('Name', validators=[InputRequired(), Length(max = 10)])
     email = StringField('Email', validators = [LENGTH_REQUIRED(min = 3, max = 20)])
@@ -45,16 +56,34 @@ def index():
 def logout():
     logout_user()
     return redirect(url_for('home.html'))
-    
-#place holder until html pages are up
-#@app.route('/Home')
-#def index():
-#    return render_template('Home.html')
-=======
+"""
+
+
+
 @app.route('/')
 def hello(name=None):
     return render_template('home.html')
 
+@app.route("/login", methods = ["POST","GET"])
+def login():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        passw = result["password"]
+        try:
+            user = authen.sign_in_with_email_and_password(email,passw)
+            print("Log in succesful")
+            return render_template('home.html') # this will be a placeholder until I get the database up and running 
+        except:
+            print("invalid")
+            return render_template('register.html')
+    else:
+        print("didn't work")
+        return render_template('login.html')
+            
+
+
+    #return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(port=1111)
+    app.run(debug=True)
