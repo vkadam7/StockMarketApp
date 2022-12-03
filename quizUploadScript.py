c214c284ae490b5624100414134b408cedc2767d
vkadam7 <go8940@wayne.edu>
2022-12-03 02:18:11 -0500

    Adding New questions, need to change

--- quizUploadScript.py
+++ quizUploadScript.py
@@ -132,7 +132,8 @@ quizName = 'Quiz2'
 db.collection('Quiz').document(quizName).set(quiz)
 
 
-questions2 = [{
+questions2 = [
+{
     #Question 1
     'text': 'What does a portfolio contain?',
     'answers': {
@@ -143,20 +144,85 @@ questions2 = [{
         },
     
     'correct': 'd'
-}
-              
+},            
 {
     #Question 2
-    'text':, 
+    'text':
     'answers': {
         'a': 
-        'b':
-        'c':
-        'd':
-    }
+        'b': ,
+        'c':, 
+        'd':,
+    },
+    
+    'correct': ''  
+},
+
+{ # Question 3
+    'text' : 'What is an ownership of a part of the companies investments?',
+    'answers' : {
+        'a' : 'Stocks',
+        'b' : 'Yields',
+        'c' : 'Volatility'
+    },
+    'correct' : 'a'
+},
+{ # Question 4
+    'text' : 'Who was the founder of stocks?',
+    'answers' : {
+        'a' : 'Benjamin Franklin',
+        'b' : 'The Dutch East India Company',
+        'c' : 'New York Stock exchange'
+    },
+    'correct' : 'b'
+},
+{ # Question 5
+    'text' : 'For dividends what is the cut off date before the record date called?',
+    'answers' : {
+        'a' : 'Dividend date',
+        'b' : 'Ex-dividend date',
+        'c' : 'Dividend reinvestment date'
+    },
+    'correct' : 'b'
+}, 
+{ # Question 6
+    'text' : 'Which of the following usually have high liquidity',
+    'answers' : {
+        'a' : 'Cash & stocks',
+        'b' : 'Real estates',
+        'c' : 'Interest rates'
+    },
+    'correct' : 'a'
+},
+{ # Question 7
+    'text' : 'What is the highest point of a companys price called?',
+    'answers' : {
+        'a' : 'Open',
+        'b' : 'Max',
+        'c' : 'High'
+    },
+    'correct' : 'c'
+}, 
+{ # Question 8
+    'text' : 'Stock graphs can be displayed as',
+    'answers' : {
+        'a' : 'Line charts',
+        'b' : 'Bar charts',
+        'c' : 'Pie charts',
+        'd' : 'all of the above',
+        'e' : 'a & b only'
+    },
+    'correct' : 'e'
+},
+{ # Question 9
+    'text' : 'What is the ticker for the Walt Disney company?',
+    'answers' : {
+        'a' : 'DISNE',
+        'b' : 'DI',
+        'c' : 'DISN',
+        'd' : 'DIS'
+    },
+    'correct' : 'd'
 }
-              
-              
-              
-              ]
+]
 
