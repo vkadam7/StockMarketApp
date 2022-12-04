ed9358b3cb6a54db4cc27737f9215bb57c307ebb
miqdadhafiz35 <98671358+miqdadhafiz35@users.noreply.github.com>
2022-12-04 17:07:49 -0500

    small fix

--- main.py
+++ main.py
@@ -149,20 +149,20 @@ def postDelete():
 #Author: Miqdad Hafiz
 @app.route("/editPost",methods = ["POST","GET"])
 def editPost():
-    session['postID'] = request.args['postID']
-    edit = dbfire.collection('Blog').document(session['postID']).get()
-    edit = edit.to_dict()
-    try:
+    if('user' in session):
+        session['postID'] = request.args['postID']
+        edit = dbfire.collection('Blog').document(session['postID']).get()
+        edit = edit.to_dict()
         edit['DocID'] = id
         return render_template("editingPost.html", edit = edit)
-    except:
-        return redirect(url_for(id))
+        
 
 @app.route("/editingPost", methods = ["POST","GET"])
 def editingPost():
     if('user' in session):
         if(request.method == "POST"):
             result = request.form
+            id = session['postID']
             editedPost = result["editingthePost"]
             dbfire.collection('Blog').document(id).update({'Post': editedPost,'DatePosted':firestore.SERVER_TIMESTAMP})
             flash("Post has been updated.")
